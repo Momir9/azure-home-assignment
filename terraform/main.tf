9d20cc805c7d0b687db5d4197af79998d02cb39f
Momir <macesic91@gmail.com>
2026-09-14 01:37:59 +0200

    source_image_reference changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,10 +87,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_reference {
+   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "24.04-LTS"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
 }
